dd146c26262ba2eb8e61f2c65271cf49aa4ba61b
Hunghgjp <hunghgjp09@gmail.com>
2024-04-03 16:41:30 +0700

    Add project files.

---
+++ Data/hrnContext.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using hrn.Models;
+
+namespace hrn.Data
+{
+    public class hrnContext : DbContext
+    {
+        public hrnContext (DbContextOptions<hrnContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<hrn.Models.Employee> Employee { get; set; } = default!;
+    }
+}
